4aed59157b102884c9fa084752cb265caa71270d
Jooho Lee <jlee@redhat.com>
2018-09-28 09:55:53 -0400

    add noRedis js

--- server-redis.js
+++ server-redis.js
@@ -6,51 +6,46 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 const redis = require('redis');
 
+const REDIS_URL = 'redis';
+const REDIS_PW = 'redis';
 const AUTH_URL = 'auth';
+// Redis
+// create and connect redis client to local instance.
+const client = redis.createClient(6379, REDIS_URL, {password: REDIS_PW});
 
-// const REDIS_URL = 'redis';
-// const REDIS_PW = 'redis';
+const room_name = 'tonronto_cc';
+var chat_members = [];
+var chat_msgs = [];
+client.on('ready', function () {
 
-// // Redis
-// // create and connect redis client to local instance.
-// const client = redis.createClient(6379, REDIS_URL, {password: REDIS_PW});
-//
-// const room_name = 'tonronto_cc';
+  console.log('redis connected')
+    // Flush Redis DB
+    // client.flushdb();
 
-// client.on('ready', function () {
-//
-//   console.log('redis connected')
-//     // Flush Redis DB
-//     // client.flushdb();
-//
-//
-//
-// // Initialize User/Msgs
-//     client.get(room_name, function (err, reply) {
-//
-//         if (reply) {
-//             chat_members = JSON.parse(reply).chat_members;
-//             chat_members = JSON.parse(reply).chat_msgs;
-//         }
-//
-//     });
-//
-//
-// })
-//
-// // Print redis errors to the console
-// client.on('error', (err) => {
-//     console.log("Error " + err);
-// });
+
+
+// Initialize User/Msgs
+    client.get(room_name, function (err, reply) {
+
+        if (reply) {
+            chat_members = JSON.parse(reply).chat_members;
+            chat_members = JSON.parse(reply).chat_msgs;
+        }
+
+    });
+
+
+})
+
+// Print redis errors to the console
+client.on('error', (err) => {
+    console.log("Error " + err);
+});
 
 //
 // client.get(room_name, function (err, result) {
 //     console.log(JSON.parse(result).chat_msgs);
 // });
-
-
-var chat_members = [];
-var chat_msgs = [];
 
 app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
@@ -81,7 +76,7 @@ app.get('/login', function (req, res, next) {
         if (auth && chat_members.indexOf(id) === -1) {
             chat_members.push(id);
 
-            // client.set(room_name, formattedData(chat_members, chat_msgs));
+            client.set(room_name, formattedData(chat_members, chat_msgs));
             res.send({
                 'chat_members': chat_members,
                 'status': 'OK'
@@ -93,9 +88,9 @@ app.get('/login', function (req, res, next) {
             });
         }
 
-        // client.get(room_name, function (err, result) {
-        //     console.log(JSON.parse(result));
-        // });
+        client.get(room_name, function (err, result) {
+            console.log(JSON.parse(result));
+        });
     });
 
 
@@ -107,7 +102,7 @@ app.get('/join', function (req, res, next) {
     console.log(nickName);
     if (chat_members.indexOf(nickName) === -1) {
         chat_members.push(nickName);
-        // client.set(room_name, formattedData(chat_members, chat_msgs));
+        client.set(room_name, formattedData(chat_members, chat_msgs));
 
         res.send({
             'chat_members': chat_members,
@@ -119,9 +114,9 @@ app.get('/join', function (req, res, next) {
         });
     }
     console.log(chat_members);console.log(chat_msgs);
-    // client.get(room_name, function (err, result) {
-    //     console.log(JSON.parse(result));
-    // });
+    client.get(room_name, function (err, result) {
+        console.log(JSON.parse(result));
+    });
 });
 
 
@@ -130,7 +125,7 @@ app.get('/leave', function (req, res, next) {
 
     if (chat_members.indexOf(nickName) > -1) {
         chat_members.splice(chat_members.indexOf(nickName), 1);
-        // client.set(room_name, formattedData(chat_members, chat_msgs));
+        client.set(room_name, formattedData(chat_members, chat_msgs));
         console.log('leave: ' + nickName);
         res.send({
             'status': 'OK'
@@ -149,16 +144,16 @@ io.on('connection', function (socket) {
     socket.on('add-message', (nickName, message) => {
         const timeStamp = new Date().getTime();
         chat_msgs.push({sender:nickName, msg: message, date: timeStamp});
-        // client.set(room_name, formattedData(chat_members, chat_msgs));
+        client.set(room_name, formattedData(chat_members, chat_msgs));
         io.emit('message', {
 
             sender: nickName,
             msg: message,
             date: timeStamp
         });
-        // client.get(room_name, function (err, result) {
-        //     console.log(JSON.parse(result));
-        // });
+        client.get(room_name, function (err, result) {
+            console.log(JSON.parse(result));
+        });
 
         console.log(message);
     });
@@ -183,6 +178,8 @@ app.get('/healthz', function (req, res) {
 
 
 function formattedData(chat_members, chat_msgs) {
+
+
     return JSON.stringify({
         "chat_members": chat_members,
         "chat_msgs": chat_msgs
