3f820b6940d2ee14315136dbc01d367d5f31cc63
Jooho Lee <jlee@redhat.com>
2018-09-29 23:00:57 -0400

    redis

--- server.js
+++ server.js
@@ -52,7 +52,7 @@ const AUTH_URL = 'auth';
 var chat_members = [];
 var chat_msgs = [];
 
-app.use(function (req, res, next) {
+app.all('/*', function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     res.header("Access-Control-Allow-Credentials", "true");
