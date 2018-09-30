ea573a31dcd3d5ca796dbccda5aad193f77da7fd
Jooho Lee <jlee@redhat.com>
2018-09-29 22:18:10 -0400

    add credentials

--- server-redis.js
+++ server-redis.js
@@ -40,6 +40,7 @@ client.on('error', (err) => {
 app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Credentials", "true");
     next();
 });
 
