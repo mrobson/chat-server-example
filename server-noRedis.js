4f06bcd4152f488cb5ae31b93372a497d8138390
Jooho Lee <jlee@redhat.com>
2018-09-29 22:19:27 -0400

    redis

--- server-noRedis.js
+++ server-noRedis.js
@@ -55,6 +55,7 @@ var chat_msgs = [];
 app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Credentials", "true");
     next();
 });
 
